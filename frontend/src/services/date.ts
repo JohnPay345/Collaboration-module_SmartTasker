23991e2c15951a68bdb1fd68bd25aa3b200256bf
John <aleshaserg05@mail.ru>
2026-03-09 15:06:01 +0500

    Исправили ошибки типов typescript добавив указания к параметрами и используя функцию Number() для преобразования строк в числа

--- frontend/src/services/date.ts
+++ frontend/src/services/date.ts
@@ -1,4 +1,4 @@
-export const parseCustomDate = (dateString) => {
+export const parseCustomDate = (dateString: string) => {
   const [day, month, year] = dateString.split('.');
-  return new Date(year, month - 1, day);
+  return new Date(Number(year), Number(month) - 1, Number(day));
 }

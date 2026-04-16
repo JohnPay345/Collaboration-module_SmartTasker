54ecb0e3de76504a53aa03a0a2aecf2c01900092
John <aleshaserg05@mail.ru>
2026-04-16 10:58:29 +0500

    backend В модели tasks переработан метод получения задач по user_id, а именно запрос - опции. Для insert-data добавлены новые поля для user_devices при вставке в соответствии с таблицей user_devices

--- backend/psql-config/insert-data.sql
+++ backend/psql-config/insert-data.sql
@@ -22,11 +22,13 @@ INSERT INTO users (first_name, middle_name, last_name, email, phone_number, pass
 'г. Красноярск, пр. Мира, д. 10', 'PR-менеджер', '/uploads/avatars/svetlana_avatar.svg', ARRAY['СМИ', 'Пресс-релизы', 'Коммуникации'], NULL, NOW(), DEFAULT);
 
 -- Вставляем данные в user_devices
-INSERT INTO user_devices (user_id, device_type, device_token)
+INSERT INTO user_devices (user_id, device_type, device_token, created_at, updated_at)
 SELECT
   user_id,
   'android',  -- Случайный выбор типа устройства
-  md5(random()::text) -- device_token (фиктивный токен)
+  md5(random()::text), -- device_token (фиктивный токен)
+  NOW(),
+  NOW()
 FROM users;
 
 -- Вставляем данные в user_notifications_settings
